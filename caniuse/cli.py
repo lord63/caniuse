4af6c95572fee0e6284846eb5cc620d2de2baca3
lord63 <lord63.j@gmail.com>
2015-10-23 08:25:53 +0800

    Add version info and add python3.5 in setup.py
    close #3

--- caniuse/cli.py
+++ caniuse/cli.py
@@ -6,9 +6,11 @@ from __future__ import absolute_import
 import click
 
 from caniuse.main import check
+from caniuse import __version__
 
 
-@click.command()
+@click.command(context_settings={'help_option_names': ('-h', '--help')})
 @click.argument('name')
+@click.version_option(__version__, '-V', '--version', message='%(version)s')
 def cli(name):
     click.echo(check(name))
